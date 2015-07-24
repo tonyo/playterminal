46d2e325d801964c3ca6d68712cfffb00974710c
Anton Ovchinnikov <revolver112@gmail.com>
2015-07-24 23:43:09 +0200

    Add the attempt liveness check

--- games/api.py
+++ games/api.py
@@ -16,13 +16,22 @@ def games(request):
     game_id = data.get('id')
     game = get_object_or_404(Game, id=game_id)
     stepic_client = StepicClient(login=STEPIC_LOGIN, password=STEPIC_PASSWORD)
-    attempt = stepic_client.create_attempt(game.step_id)
-
-    # Save attempt id for future use
-    attempt['dataset']['stepic_attempt_id'] = attempt['id']
     game_sessions_map = request.session.get('game_sessions', {})
-    if not str(game_id) in game_sessions_map:
+
+    request_new_attempt = True
+    if str(game_id) in game_sessions_map:
+        # Check that the attempt is still active
+        attempt_id = game_sessions_map[str(game_id)]['stepic_attempt_id']
+        remote_attempt = stepic_client.get_attempt(attempt_id)
+        if remote_attempt['status'] == 'active':
+            request_new_attempt = False
+
+    if request_new_attempt:
+        attempt = stepic_client.create_attempt(game.step_id)
+        # Save attempt id for future use
+        attempt['dataset']['stepic_attempt_id'] = attempt['id']
         game_sessions_map[str(game_id)] = attempt['dataset']
+
     game_session = game_sessions_map[str(game_id)]
     request.session['game_sessions'] = game_sessions_map
     return JsonResponse(game_session)
--- tests/games/test_api.py
+++ tests/games/test_api.py
@@ -1,18 +1,41 @@
 import json
+
+from django.contrib.sessions.backends.db import SessionStore
 from django.core.urlresolvers import reverse
 
 
+def _post_json(client, url, data, **kwargs):
+    return client.post(url, content_type='application/json',
+                       data=json.dumps(data), **kwargs)
+
+
 def test_new_game_session(game, client):
-    response = client.post(reverse('games'), content_type='application/json',
-                           data=json.dumps({'id': game.id}))
+    response = _post_json(client, reverse('games'), {'id': game.id})
 
     game_session = json.loads(response.content.decode())
     assert game_session['kaylee_url']
     assert game_session['terminal_id']
 
-    # request a new game and expect the previously created game is returned
-    response = client.post(reverse('games'), content_type='application/json',
-                           data=json.dumps({'id': game.id}))
+    # Request once again, expect that the previous session is returned
+    response = _post_json(client, reverse('games'), {'id': game.id})
 
     new_game_session = json.loads(response.content.decode())
     assert new_game_session == game_session
+
+
+def test_outdated_game_session(game, client):
+    _post_json(client, reverse('games'), {'id': game.id})
+
+    # Update the game session
+    invalid_attempt_id = 4729799
+    session_object = SessionStore(session_key=client.session.session_key)
+    invalid_game_session = session_object['game_sessions'][str(game.id)]
+    invalid_game_session['stepic_attempt_id'] = invalid_attempt_id
+    session_object.save()
+
+    # Repeat request
+    response = _post_json(client, reverse('games'), {'id': game.id})
+    new_game_session = json.loads(response.content.decode())
+
+    # Check that the new session is returned
+    assert new_game_session != invalid_game_session
