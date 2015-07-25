0cc2c4f1a5d6ee0dacc53be7aaac8eb97104a0c7
Anton Ovchinnikov <revolver112@gmail.com>
2015-07-25 02:55:44 +0200

    Add Play click handling

---
+++ games/static/games/js/playterminal.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $('#open-terminal-btn').click(function() {
+        $.ajax({
+            url: gamesApiUrl,
+            dataType: "json",
+            method: "POST",
+            data: '{"id": ' + gameId + '}',
+            contentType: 'application/json',
+            beforeSend: function(request)
+            {
+                request.setRequestHeader("X-CSRFToken", csrfToken);
+            }
+        }).done(function(result) {
+            tty.open(result["kaylee_url"], result["terminal_id"]);
+        });
+    });
+});
