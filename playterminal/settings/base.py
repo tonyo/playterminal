d5eb9d73af0e5aacff6daeba0ca1a993cbb7215c
Anton Ovchinnikov <revolver112@gmail.com>
2016-07-17 00:54:44 +0200

    Use gunicorn instead of uwsgi

--- playterminal/settings/base.py
+++ playterminal/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'playtermin.al', 'pt']
 
 
 # Application definition
