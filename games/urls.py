a9466bf963dbdabd06a1ae1b6ced0d4ff2d5f384
Anton Ovchinnikov <revolver112@gmail.com>
2016-10-28 22:39:19 +0200

    Add a link to the main page

--- games/urls.py
+++ games/urls.py
@@ -1,21 +1,23 @@
 from django.conf.urls import url
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.views.generic import TemplateView
 from django.contrib.flatpages import views as flatpages_views
 
 from games import views
 from games.api import terminals
 
 urlpatterns = [
-    url(r'^$', lambda _: HttpResponseRedirect(reverse('game_list'))),
+    url(r'^$', lambda _: HttpResponseRedirect(reverse('main'))),
     url(r'^game/(?P<pk>\d+)/$',
         views.GameDetailView.as_view(), name='game_detail'),
     url(r'^games/$',
         views.GameListView.as_view(), name='game_list'),
+    url(r'^main/$',
+        flatpages_views.flatpage, {'url': '/main/'}, name='main'),
     url(r'^about/$',
         flatpages_views.flatpage, {'url': '/about/'}, name='about'),
 
+    # API
     url(r'^api/terminals/$',
         terminals, name='terminals'),
 ]
